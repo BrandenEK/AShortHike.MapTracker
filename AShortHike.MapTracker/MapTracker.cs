c8d3ce4ebe6df3819333e76411b6eb00846861fd
unknown <brandenek03912@hotmail.com>
2024-04-21 14:24:02 -0400

    core: better properties

--- AShortHike.MapTracker/MapTracker.cs
+++ AShortHike.MapTracker/MapTracker.cs
@@ -8,11 +8,6 @@ public class MapTracker : ShortHikeMod
 {
     public MapTracker() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    private RectTransform _mapObject = null;
-    private RectTransform _characterObject = null;
-
-    private Transform _canvasObject = null;
-
     protected override void OnInitialize()
     {
         LogHandler.Error($"{ModInfo.MOD_NAME} has been initialized");
@@ -30,28 +25,12 @@ public class MapTracker : ShortHikeMod
     {
         LogHandler.Info("Toggling map");
 
-        if (_mapObject == null)
-            CreateMap();
-
-        _mapObject.gameObject.SetActive(!_mapObject.gameObject.activeSelf);
+        Map.gameObject.SetActive(!Map.gameObject.activeSelf);
     }
 
     private void UpdatePlayerPosition()
     {
 
-    }
-
-    private void CreateMap()
-    {
-        if (_canvasObject == null)
-            _canvasObject = CreateCanvas().transform;
-
-        RectTransform borderImage = CreateImage("Border", _canvasObject, new Vector2(910, 910), null, new Color(242 / (float)255, 238 / (float)255, 203 / (float)255));
-        RectTransform mapImage = CreateImage("Map", borderImage, new Vector2(900, 900), null, Color.red);
-        RectTransform character = CreateImage("Character", mapImage, new Vector2(50, 50), null, Color.yellow);
-
-        _mapObject = borderImage;
-        _characterObject = character;
     }
 
     private RectTransform CreateImage(string name, Transform parent, Vector2 size, Sprite sprite, Color color)
@@ -67,20 +46,46 @@ public class MapTracker : ShortHikeMod
         return rect;
     }
 
-    private Canvas CreateCanvas()
+    private Transform x_map;
+    private Transform Map
     {
-        GameObject obj = new("Better canvas");
-        obj.layer = LayerMask.NameToLayer("UI");
-        obj.transform.parent = Main.TransformHolder;
+        get
+        {
+            if (x_map != null)
+                return x_map;
 
-        Canvas canvas = obj.AddComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            RectTransform borderImage = CreateImage("Border", Canvas, new Vector2(910, 910), null, new Color(242 / (float)255, 238 / (float)255, 203 / (float)255));
+            RectTransform mapImage = CreateImage("Map", borderImage, new Vector2(900, 900), null, Color.red);
+            RectTransform character = CreateImage("Character", mapImage, new Vector2(50, 50), null, Color.yellow);
+            borderImage.gameObject.SetActive(false);
 
-        CanvasScaler scaler = obj.AddComponent<CanvasScaler>();
-        scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-        scaler.referenceResolution = new Vector2(1920, 1080);
-        scaler.referencePixelsPerUnit = 100;
+            LogHandler.Warning("Created new map object");
+            return x_map = borderImage;
+        }
+    }
 
-        return canvas;
+    private Transform x_canvas;
+    private Transform Canvas
+    {
+        get
+        {
+            if (x_canvas != null)
+                return x_canvas;
+
+            GameObject obj = new("Better canvas");
+            obj.layer = LayerMask.NameToLayer("UI");
+            obj.transform.parent = Main.TransformHolder;
+
+            Canvas canvas = obj.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            CanvasScaler scaler = obj.AddComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            scaler.referenceResolution = new Vector2(1920, 1080);
+            scaler.referencePixelsPerUnit = 100;
+
+            LogHandler.Warning("Created new canvas object");
+            return x_canvas = canvas.transform;
+        }
     }
 }
