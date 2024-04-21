a6689ffec6bd43222e5bff139044a51c41c89e95
unknown <brandenek03912@hotmail.com>
2024-04-21 13:11:23 -0400

    init: add base project files

---
+++ AShortHike.MapTracker/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace AShortHike.MapTracker;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("AShortHike.ModdingAPI", "1.0.0")]
+public class Main : BaseUnityPlugin
+{
+    public static MapTracker MapTracker { get; private set; }
+
+    private void Awake()
+    {
+        MapTracker = new MapTracker();
+    }
+}
---
+++ AShortHike.MapTracker/MapTracker.cs
@@ -0,0 +1,13 @@
+﻿using AShortHike.ModdingAPI;
+
+namespace AShortHike.MapTracker;
+
+public class MapTracker : ShortHikeMod
+{
+    public MapTracker() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogHandler.Error($"{ModInfo.MOD_NAME} has been initialized");
+    }
+}
